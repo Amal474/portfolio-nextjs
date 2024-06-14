d1b102e1d905fdb72541ba236c0b678449aad725
Amal474 <mohammedamal.n@gmail.com>
2024-06-14 17:16:37 +0300

    Nav test

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,25 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+// Custom Components
+import Navbar from "./Navbar";
+
+
 export default function Header() {
   return (
-    <header>Header</header>
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-4xl font-semibold">
+          Mohammed Amal N
+        </h1>
+        <div className="hidden md:flex">
+          <Navbar/>
+          <Link href={"/contact"}>
+            <Button>Contact Me</Button>
+          </Link>
+        </div>
+        <div className="md:hidden">Mobile Nav</div>
+      </div>
+    </header>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+
+export default function Navbar() {
+  return (
+    <nav>Nav BAr</nav>
+  );
+}
