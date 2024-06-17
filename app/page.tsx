e1b31299e133bc1c3200b2d0ebda2ccc654eadc2
Amal474 <mohammedamal.n@gmail.com>
2024-06-17 18:28:06 +0300

    [FIX] Home Page Base

--- app/page.tsx
+++ app/page.tsx
@@ -14,12 +14,12 @@ export default function Home() {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1">
-              Hello I'm, 
+              Hello I&apos;m, 
               <br/>
               <span className="text-accent">Mohammed Amal</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-              I'm a software developer and an ERP techno-functional consultant. I'm proficient in various prgramming languages
+              I&apos;m a software developer and an ERP techno-functional consultant. I'm proficient in various prgramming languages
               and technologies.
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
